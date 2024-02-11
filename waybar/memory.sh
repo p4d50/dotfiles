49dbacd9455d8663c991b877c9c9f5f7752ef8f9
Veljko S. <p4d50@proton.me>
2024-02-11 15:52:13 +0100

    change waybar ram module format

--- waybar/memory.sh
+++ waybar/memory.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-total=$(free -h --si | awk '/^Mem/ {print $2}')
-used=$(free -h --si | awk '/^Mem/ {print $3}')
+total=$(free -h --si | awk '/^Mem/ {print $2}' | sed 's/G/GB/g; s/M/MB/g')
+used=$(free -h --si | awk '/^Mem/ {print $3}' | sed 's/G/GB/g; s/M/MB/g')
 
 echo -n "{\"text\": \"<span weight='600'>$used</span> / <span color='#aaaaaa'>$total</span>\" }"
