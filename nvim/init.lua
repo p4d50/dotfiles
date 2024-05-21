3eef0056fea0fad36751dccde12b86f080694007
p4d50 <p4d50@proton.me>
2024-05-21 23:34:04 +0200

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require('lazy').setup({
   --
   -- UI STUFF
   --
@@ -30,12 +30,6 @@ require("lazy").setup({
 
       vim.cmd.colorscheme('night-owl')
     end,
-  },
-
-  -- better ui
-  {
-    'stevearc/dressing.nvim',
-    event = "VeryLazy",
   },
 
   -- lualine
@@ -84,9 +78,6 @@ require("lazy").setup({
           vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
         end
       })
-
-      vim.keymap.set('n', '<C-p>', ':NvimTreeToggle<CR>', { noremap = true })
-      vim.keymap.set('n', '<leader>l', ':NvimTreeFindFile!<CR>', { noremap = true })
     end,
   },
 
@@ -96,6 +87,15 @@ require("lazy").setup({
       -- options
     },
   },
+
+  {
+    'm4xshen/autoclose.nvim',
+    config = function()
+      require("autoclose").setup()
+    end
+  },
+
+  { 'bronson/vim-visual-star-search' },
 
   --
   -- BETTER TEXT MANIPULATION
@@ -172,6 +172,7 @@ require("lazy").setup({
           'surface',
           'lua',
           'bash',
+          'php',
           'markdown',
           'markdown_inline',
         },
@@ -251,6 +252,8 @@ require("lazy").setup({
       })
     end,
   },
+  
+  { 'mbbill/undotree' },
 
   --
   -- Autocompletion & lsp stuff 
@@ -268,6 +271,14 @@ require("lazy").setup({
       local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
       local capabilities = cmp_nvim_lsp.default_capabilities()
+
+      lspconfig.phpactor.setup {
+        on_attach = on_attach,
+        init_options = {
+          ["language_server_phpstan.enabled"] = false,
+          ["language_Server_psalm.enalbed"] = false,
+        }
+      }
 
       local lexical_config = {
         filetypes = { "elixir", "eelixir", "heex" },
@@ -292,6 +303,41 @@ require("lazy").setup({
       lspconfig.lexical.setup({
         capabilities = capabilities,
       })
+
+      -- Lsp keymaps
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
     end
   },
 
@@ -308,6 +354,18 @@ require("lazy").setup({
     config = function ()
       local cmp = require('cmp')
       local luasnip = require('luasnip')
+
+      vim.keymap.set({ "i", "s" }, "<c-k>", function()
+        if luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, { silent = true })
+
+      vim.keymap.set({ "i", "s" }, "<c-j>", function()
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { silent = true })
 
       require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -337,10 +395,10 @@ require("lazy").setup({
         }),
       })
     end
-  }
+  },
 })
 
--- disable netrw at the very start of our init.lua, because we use nvim-tree
+-- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -375,7 +433,9 @@ vim.g.mapleader = ','
 -- keymaps
 local keymap = vim.keymap
 
-keymap.set('n', '<leader>nh', ':nohl<CR>') -- delete highlight search
+vim.keymap.set('i', 'jk', '<Esc>')
+
+keymap.set('n', '<leader>nh', ':nohl<CR>')
 keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>')
 keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>')
 
@@ -389,3 +449,28 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
+
+-- visual mode section moving
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- search terms stay in middle
+vim.keymap.set('n', 'n', "nzzzv")
+vim.keymap.set('n', 'N', "Nzzzv")
+
+-- replace word in whole file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set('n', '<leader>U', vim.cmd.UndotreeToggle)
+
+vim.keymap.set({'n', 'x', 'o'}, 'H', '^')
+vim.keymap.set({'n', 'x', 'o'}, 'L', '$')
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set('n', '<C-p>', ':NvimTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>l', ':NvimTreeFindFile!<CR>', { noremap = true })
+
+-- Yank whole line
+vim.keymap.set('n', 'Y', 'y$')
